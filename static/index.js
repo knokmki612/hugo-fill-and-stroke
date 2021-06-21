d8c54acb0f7b054525630702364e48a3d4c7d61c
Kimiaki Kuno <knokmki612@gmail.com>
2021-06-22 05:42:58 +0900

    fix: twitter widget not working with pushState

--- static/index.js
+++ static/index.js
@@ -2,14 +2,24 @@ const swup = new Swup({
   linkSelector: `a[href^="${window.location.origin}"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])`,
   plugins: [
     new SwupScrollPlugin({
-      animateScroll: false
+      animateScroll: false,
     }),
-    new SwupPreloadPlugin()
-  ]
+    new SwupPreloadPlugin(),
+  ],
 });
-if(document.getElementsByClassName("twitter-tweet").length > 0) {
-  var script = document.createElement("script");
-  script.async = true;
-  script.src = "https://platform.twitter.com/widgets.js";
-  document.body.appendChild(script);
-}
+
+const loadTwitter = () => {
+  if (document.getElementsByClassName("twitter-tweet").length === 0) return;
+  if (!window.twttr) {
+    const script = document.createElement("script");
+    script.defer = true;
+    script.src = "https://platform.twitter.com/widgets.js";
+    document.body.appendChild(script);
+  } else {
+    window.twttr.widgets.load();
+  }
+};
+
+swup.on("pageView", loadTwitter);
+
+loadTwitter();
