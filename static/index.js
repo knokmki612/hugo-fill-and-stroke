3e8772c543ff6b7ec912c616556775ce49e561c9
Kimiaki Kuno <knokmki612@gmail.com>
2021-07-21 17:43:18 +0900

    fix: not working anchor link with swup.js

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 const swup = new Swup({
-  linkSelector: `a[href^="${window.location.origin}"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])`,
+  linkSelector: `a[href^="${window.location.origin}"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([[data-no-swup]])`,
   plugins: [
     new SwupScrollPlugin({
       animateScroll: false,
